d3c5ce86f2200a705a4ed520c04cb9a0e1c8bf02
krlkonstantine <krlkonstantine@gmail.com>
2023-11-08 10:21:26 +0300

    got characters names

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {API} from "../../assets/api/api";
 import {ResponseType, CharacterType} from "../../assets/api/rick-and-morty-api";
+import {Header} from "../../components/Header/Header";
+import Image from "next/image";
+import {PageWrapper} from "../../components/PageWrapper/PageWrapper";
 
-const getStaticProps = async ()=>{
-        const characters = await API.rickAndMorty.getCharacters()
+export const getStaticProps = async () => {
+    const characters = await API.rickAndMorty.getCharacters()
 
-    return  {
-        props:{
+    return {
+        props: {
             characters
         }
     }
@@ -18,10 +21,18 @@ type PropsType = {
 }
 
 const Characters = (props: PropsType) => {
+    const {characters} = props
+
+    const charactersList = characters.results.map(character =>
+        <div key={character.id}>
+            {character.name}
+        </div>
+    )
     return (
-        <>
-            Characters
-        </>
+        <PageWrapper>
+            <Header/>
+            {charactersList}
+        </PageWrapper>
     );
 };
 
