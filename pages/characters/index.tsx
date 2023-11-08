aeb7e6c89038873bd8cf5f276dae7952d86f645b
krlkonstantine <krlkonstantine@gmail.com>
2023-11-08 10:05:44 +0300

    props added

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import {API} from "../../assets/api/api";
+import {ResponseType, CharacterType} from "../../assets/api/rick-and-morty-api";
 
-const Characters = () => {
+const getStaticProps = async ()=>{
+        const characters = await API.rickAndMorty.getCharacters()
+
+    return  {
+        props:{
+            characters
+        }
+    }
+}
+//получаем персонажей с апишки, возвращаем их  в объекте пропс
+//далее типизируем и отдаем объект компоненту
+type PropsType = {
+    characters: ResponseType<CharacterType>
+}
+
+const Characters = (props: PropsType) => {
     return (
         <>
             Characters
