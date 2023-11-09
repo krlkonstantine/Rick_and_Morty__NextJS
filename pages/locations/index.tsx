0db89e908f27a7bd9916af39b387fb1576d3dd6c
krlkonstantine <krlkonstantine@gmail.com>
2023-11-09 09:24:42 +0300

    dehydrating static props implemented

--- pages/locations/index.tsx
+++ pages/locations/index.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 
 import {Header} from "../../components/Header/Header";
 import {PageWrapper} from "../../components/PageWrapper/PageWrapper";
-import {useQuery} from "@tanstack/react-query";
+import {dehydrate, QueryClient, useQuery} from "@tanstack/react-query";
 import {ResponseType, LocationType} from "../../assets/api/rick-and-morty-api";
+import {API} from "../../assets/api/api";
 
 const getLocations = () => {
     return fetch('https://rickandmortyapi.com/api/location',
         {method: 'GET'}).then(res => res.json())
+}
+
+export const getStaticProps = async () => {
+    const queryClient = new QueryClient()
+
+    await queryClient.fetchQuery(['locations'], getLocations)
+
+
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient)
+        }
+    }
 }
 
 const Locations = () => {
