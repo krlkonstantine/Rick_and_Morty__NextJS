02dcd4f9a69037bbdf0254e741279317c87e6006
krlkonstantine <krlkonstantine@gmail.com>
2023-11-14 09:33:12 +0300

    feat: getStaticPath added, character is being correctly displayed

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import {API} from "../../assets/api/api";
-import { CharacterType} from "../../assets/api/rick-and-morty-api";
+import {CharacterType} from "../../assets/api/rick-and-morty-api";
 import {PageWrapper} from "../../components/PageWrapper/PageWrapper";
 import {CharacterCard} from "../../components/Card/CharacterCard/CharacterCard";
 import {getLayout} from "../../components/Layout/BaseLayout/BaseLayout";
-import {GetStaticProps} from "next";
+import {GetStaticPaths, GetStaticProps} from "next";
 
+export const getStaticPaths: GetStaticPaths = async () => {
+    //тоже асинк фнк, вызывается на сервере и используется
+    // для вызывания нескольких статических страниц сразу
+    //если мы знаем что 20 персонажей и хоти сгенерить
+    //заранее странички для лучшего пользов.опыта, чтобы
+    //наши стр быстрее отдавались и кешировались
+    //работает в связке с гетСтатикПропс, генерирует пути
+    //для предзапроса даннах, а гСтатПропс генерит саму страничку
+    //должен возвращать массив путей
+    const {results} = await API.rickAndMorty.getCharacters()
 
+    const paths = results.map(character => (
+        {params: {id: String(character.id)}}
+    ))
+   //должен возвращать массив объектов парамс у каждого из которых обж айди
+
+    return {
+        paths,
+        fallback: false
+    }
+    //что поулчаем в итоге: мы сгенерили characters, присволи их result
+    //далее мы мапим каждого перса, и для каждого создаем объект params,
+    //содержащий в др.объекте его айди. Все эти объекты
+    //запаковываем в массив и каждый оъект по сути это путь
+    //по которому будем запрашивать конкретнго перса заранее
+}
 export const getStaticProps: GetStaticProps<PropsType> = async ({params}) => {
     //типизируем фнк (: GetStaticProps) чтобы у нас протипизировался обж
     //приходщий в виде параметров. По факту прих.контекст, обычно пишется ctx
